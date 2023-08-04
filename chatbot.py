1cbf41978ea7234155346886202e6701c950694d
Lucio Capezzuto <luciodimascapezzuto@gmail.com>
2023-08-04 16:51:10 -0300

    first commit

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+#Función para las respuestas predeterminadas
+def respuestas-p():
+    while  :
+        entrada = input()
+        if entrada == "Hola":
+            print("Hola, como puedo ayudarte hoy??") 
+        elif  :
+            print("No tengo capacidad de sentir nad... !!AYUDAA!!")
+        
+
+        
