928a775d2e81ad8b627962539a71b51d0af76455
Lucio Capezzuto <luciodimascapezzuto@gmail.com>
2023-08-08 16:51:58 -0300

    Punto 1 terminado

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,27 @@
-import re
+palabras = "!?¿¡#$%&/()\"]}{[-_.,:;*= '1234567890"
+bot = "Terminator"
 #Función para las respuestas predeterminadas
-def respuestas-p():
+def respuestas():
+    nombre = input(f"{bot}: ¿Como te llamas?\n You: ")
     while  True:
-        entrada-nombre = input("Como te llamas")
-    entrada = input().lower().lstrip().rstrip()
-    patron = "[\W]+"
-    regex = re.compile(patron)
-    resultado = regex.sub("",entrada)
-    print(resultado)
-    entradas = entrada.split()
-    if entrada == "hola":
-        print(f"Hola {entrada-nombre} , ¿como puedo ayudarte hoy?") 
-    elif  entrada == "como estas":
-        print("Como modelo de lenguaje no tengo capacidad de sentir nad... !!AYUDAA!!")
-    elif "chiste" in entrada:
-        print("")
-    elif entrada == "Cual es la temperarura"
-    elif entrada == "adios":
-        print(f"Adios {entrada-nombre}, nos vemos pronto")
-        return
-    else:
-        print("Lo siento, no entiendo ese comando. Por favor, prueba otra vez")
+        entrada = input(f"{nombre}: ").lower()
+        for e in palabras:
+            entrada = entrada.replace(e, '')
+        for x in range(len(entrada)):
+            if entrada[x] == entrada[x-1] and x>0:
+                del entrada[x -1]
+        if "hola" in entrada:
+            print(f"\n{bot}: Hola {nombre}, ¿como puedo ayudarte hoy?\n") 
+        elif  "comoestas" in entrada:
+            print(f"\n{bot}: Como modelo de lenguaje no tengo capacidad de sentir nad... !!AYUDAA!!\n")
+        elif "chiste" in entrada:
+            print(f"\n{bot}: jajajjaja")
+        elif "cualeslatemperatura" in entrada:
+            print(f"\n{bot}: gopo")
+        elif "adios" in entrada:
+            print(f"\n{bot}: Adios {nombre}, nos vemos pronto\n")
+            return
+        else:
+            print(f"\n{bot}: Lo siento {nombre}, no entiendo ese comando. Por favor, prueba otra vez\n")
+
+respuestas()
