356124c0f6b046dbaeaef01f16ac1ac06f701a1e
Lucio Capezzuto <luciodimascapezzuto@gmail.com>
2023-08-09 23:54:55 -0300

    Punto 2 casi terminado y corregí cosas del 1

--- chatbot.py
+++ chatbot.py
@@ -2,14 +2,12 @@
 import requests
 
 #----------------------------------------VARIABLES------------------------------------------------------
-palabras = "!?¿¡#$%&/()\"]}{[-_.,:;*= '1234567890"
+palabras = "!?¿¡#$%&/()\"]}{[-_.,:;*=+`´'1234567890"
 bot = "Terminator"
-
 #----------------------------------------FUNCIONES------------------------------------------------------
-
 #Función para el clima
 def obtener_clima(ciudad):
-    api_key = "TU_CLAVE_DE_API"  # Reemplaza con tu clave de API
+    api_key = "fbd7ef57176bf0fe9d7da1c272565abd"
     url = f"http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric"
     
     response = requests.get(url)
@@ -17,17 +15,10 @@ def obtener_clima(ciudad):
 
     if response.status_code == 200:
         temperatura = data["main"]["temp"]
-        descripcion = data["weather"][0]["description"]
-        return f"El clima en {ciudad} es {descripcion} con una temperatura de {temperatura}°C."
+        descripcion = data["weather"]
+        return f"La temperatura en {ciudad} es de {temperatura}°C."
     else:
         return "No pude obtener el pronóstico del clima en este momento."
-
-# Ejemplo de uso
-
-
-
-
-
 
 #Función para las respuestas predeterminadas
 def respuestas():
@@ -36,12 +27,21 @@ def respuestas():
         entrada = input(f"{nombre}: ").lower()
         for e in palabras:
             entrada = entrada.replace(e, '')
+        s = False
+        h = 0
         for x in range(len(entrada)):
-            if x>0 and x<len(entrada):
+            x = x -h
+            if s:
+                h = h + 1
+            s = False
+            print(x,"\n",entrada,len(entrada))
+            if x>0 and x < len(entrada):
                 if entrada[x] == entrada[x-1] :
+                    s = True
                     entrada = [s for s in entrada]
-                    del entrada[x-1]
+                    del entrada[x]
                     entrada = ''.join(entrada)
+        entrada_l = entrada.split()
         if "hola" in entrada:
             print(f"\n{bot}: Hola {nombre}, ¿como puedo ayudarte hoy?\n") 
         elif  "como" and "estas" in entrada:
@@ -49,14 +49,16 @@ def respuestas():
         elif "chiste" in entrada:
             print(f"\n{bot}: jajajjaja")
         elif "clima" in entrada:
-            
-            print(f"\n{bot}: gopo")
+            #ciudad = entrada_l[x for x in range(len(entrada_l) if len(x))>4]
+            ciudad = "Buenos Aires"
+            pronostico = obtener_clima(ciudad)
+            print(pronostico)
         elif "adios" in entrada:
             print(f"\n{bot}: Adios {nombre}, nos vemos pronto\n")
             return
         else:
             print(f"\n{bot}: Lo siento {nombre}, no entiendo ese comando. Por favor, prueba otra vez\n")
-ciudad = "NOMBRE_DE_LA_CIUDAD"
-pronostico = obtener_clima(ciudad)
-print(pronostico)
+
+#----------------------------------------Programa------------------------------------------------------
+
 respuestas()
